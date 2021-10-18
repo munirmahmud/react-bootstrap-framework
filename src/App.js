625edf47e0443ee90df26dca49831aca2e15cfdd
Munir Mahmud <munirmahmud6@gmail.com>
2021-10-18 17:22:39 +0600

    Global styles modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Route, Switch } from "react-router";
+import About from "./pages/About";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import "./bootstrap/global.styles.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById("root")
 );
