59eb52cff1e1febfeb793288a53989b4f4de8676
Munir Mahmud <munirmahmud6@gmail.com>
2021-10-24 10:41:48 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./bootstrap/global.styles.css";
