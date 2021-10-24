9873b3564e9007e26c4a637eded8fc8cacdd45ea
Munir Mahmud <munirmahmud6@gmail.com>
2021-10-24 18:06:57 +0600

    Button component updated

--- src/bootstrap/Button/index.jsx
+++ src/bootstrap/Button/index.jsx
@@ -3,20 +3,64 @@ import "./button.styles.css";
 
 export const Button = ({
   type,
-  bg,
   className,
-  spacing,
-  icon,
+  color,
   children,
+  outline,
   ...rest
 }) => {
   const btnType = type ? type : "submit";
-  const btnClass = className ? `btn btn-${className}` : "";
+  const btnClass = className ? `btn-${className}` : "btn-primary";
+
+  const btnColor =
+    color === "primary"
+      ? "btn-primary"
+      : color === "secondary"
+      ? "btn-secondary"
+      : color === "success"
+      ? "btn-success"
+      : color === "danger"
+      ? "btn-danger"
+      : color === "warning"
+      ? "btn-warning"
+      : color === "info"
+      ? "btn-info"
+      : color === "light"
+      ? "btn-light"
+      : color === "dark"
+      ? "btn-dark"
+      : color === "black"
+      ? "btn-black"
+      : color === "white"
+      ? "btn-white"
+      : "btn-primary";
+
+  const btnOutline =
+    outline === "primary"
+      ? "btn-outline-primary"
+      : outline === "secondary"
+      ? "btn-outline-secondary"
+      : outline === "success"
+      ? "btn-outline-success"
+      : outline === "danger"
+      ? "btn-outline-danger"
+      : outline === "warning"
+      ? "btn-outline-warning"
+      : outline === "info"
+      ? "btn-outline-info"
+      : outline === "light"
+      ? "btn-outline-light"
+      : outline === "dark"
+      ? "btn-outline-dark"
+      : "btn-outline-gray";
 
   return (
     <>
-      <button type={btnType} className={btnClass} {...rest}>
-        {icon && <img src={icon} alt="icon" className="icon-img" />}
+      <button
+        type={btnType}
+        className={`btn ${outline ? btnOutline : btnColor}`}
+        {...rest}
+      >
         {children}
       </button>
     </>
